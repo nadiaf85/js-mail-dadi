bd823464ea3c085a65d55ff2f2cd6f736f761f18
nadiaf85 <nadia.fiorio@alice.it>
2021-12-13 15:43:52 +0100

    modifiche dadi

--- dadi/js/myscript.js
+++ dadi/js/myscript.js
@@ -1,2 +1,11 @@
 // Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
 // Stabilire il vincitore, in base a chi fa il punteggio più alto.
+
+let utente = Math.floor(Math.random()*5+1);
+let computer = Math.floor(Math.random()*5+1);
+
+if(utente > pc){
+    document.writeln('utente vincitore');
+}else{
+    document.writeln('pc vincitore');
+}
